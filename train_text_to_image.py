1c6e82623bf1c3003b43f1afed33cf2ed02cba0f
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-13 10:01:05 -0500

    indentation

--- train_text_to_image.py
+++ train_text_to_image.py
@@ -64,6 +64,7 @@ logger = get_logger(__name__, log_level="INFO")
 
 DATASET_NAME_MAPPING = {
     "lambdalabs/pokemon-blip-captions": ("image", "text"),
+    "jlbaker361/sd-wikiart20": ("image", "text")
 }
 
 
@@ -80,61 +81,61 @@ def save_model_card(
         img_str += "![val_imgs_grid](./val_imgs_grid.png)\n"
 
     yaml = f"""
----
-license: creativeml-openrail-m
-base_model: {args.pretrained_model_name_or_path}
-datasets:
-- {args.dataset_name}
-tags:
-- stable-diffusion
-- stable-diffusion-diffusers
-- text-to-image
-- diffusers
-inference: true
----
+    ---
+    license: creativeml-openrail-m
+    base_model: {args.pretrained_model_name_or_path}
+    datasets:
+    - {args.dataset_name}
+    tags:
+    - stable-diffusion
+    - stable-diffusion-diffusers
+    - text-to-image
+    - diffusers
+    inference: true
+    ---
     """
     model_card = f"""
-# Text-to-image finetuning - {repo_id}
+    # Text-to-image finetuning - {repo_id}
 
-This pipeline was finetuned from **{args.pretrained_model_name_or_path}** on the **{args.dataset_name}** dataset. Below are some example images generated with the finetuned pipeline using the following prompts: {args.validation_prompts}: \n
-{img_str}
+    This pipeline was finetuned from **{args.pretrained_model_name_or_path}** on the **{args.dataset_name}** dataset. Below are some example images generated with the finetuned pipeline using the following prompts: {args.validation_prompts}: \n
+    {img_str}
 
-## Pipeline usage
+    ## Pipeline usage
 
-You can use the pipeline like so:
+    You can use the pipeline like so:
 
-```python
-from diffusers import DiffusionPipeline
-import torch
+    ```python
+    from diffusers import DiffusionPipeline
+    import torch
 
-pipeline = DiffusionPipeline.from_pretrained("{repo_id}", torch_dtype=torch.float16)
-prompt = "{args.validation_prompts[0]}"
-image = pipeline(prompt).images[0]
-image.save("my_image.png")
-```
+    pipeline = DiffusionPipeline.from_pretrained("{repo_id}", torch_dtype=torch.float16)
+    prompt = "{args.validation_prompts[0]}"
+    image = pipeline(prompt).images[0]
+    image.save("my_image.png")
+    ```
 
-## Training info
+    ## Training info
 
-These are the key hyperparameters used during training:
+    These are the key hyperparameters used during training:
 
-* Epochs: {args.num_train_epochs}
-* Learning rate: {args.learning_rate}
-* Batch size: {args.train_batch_size}
-* Gradient accumulation steps: {args.gradient_accumulation_steps}
-* Image resolution: {args.resolution}
-* Mixed-precision: {args.mixed_precision}
+    * Epochs: {args.num_train_epochs}
+    * Learning rate: {args.learning_rate}
+    * Batch size: {args.train_batch_size}
+    * Gradient accumulation steps: {args.gradient_accumulation_steps}
+    * Image resolution: {args.resolution}
+    * Mixed-precision: {args.mixed_precision}
 
-"""
+    """
     wandb_info = ""
     if is_wandb_available():
         wandb_run_url = None
         if wandb.run is not None:
             wandb_run_url = wandb.run.url
 
-    if wandb_run_url is not None:
-        wandb_info = f"""
-More information on all the CLI arguments and the environment are available on your [`wandb` run page]({wandb_run_url}).
-"""
+        if wandb_run_url is not None:
+            wandb_info = f"""
+        More information on all the CLI arguments and the environment are available on your [`wandb` run page]({wandb_run_url}).
+        """
 
     model_card += wandb_info
 
