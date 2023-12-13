b51afbf9740cdde01ceab8ac42ab40f18d6bfc44
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-13 10:01:30 -0500

    indentation

--- train_text_to_image_lora.py
+++ train_text_to_image_lora.py
@@ -99,23 +99,23 @@ def save_model_card(repo_id: str, images=None, base_model=str, dataset_name=str,
         img_str += f"![img_{i}](./image_{i}.png)\n"
 
     yaml = f"""
----
-license: creativeml-openrail-m
-base_model: {base_model}
-tags:
-- stable-diffusion
-- stable-diffusion-diffusers
-- text-to-image
-- diffusers
-- lora
-inference: true
----
+    ---
+    license: creativeml-openrail-m
+    base_model: {base_model}
+    tags:
+    - stable-diffusion
+    - stable-diffusion-diffusers
+    - text-to-image
+    - diffusers
+    - lora
+    inference: true
+    ---
+        """
+        model_card = f"""
+    # LoRA text2image fine-tuning - {repo_id}
+    These are LoRA adaption weights for {base_model}. The weights were fine-tuned on the {dataset_name} dataset. You can find some example images in the following. \n
+    {img_str}
     """
-    model_card = f"""
-# LoRA text2image fine-tuning - {repo_id}
-These are LoRA adaption weights for {base_model}. The weights were fine-tuned on the {dataset_name} dataset. You can find some example images in the following. \n
-{img_str}
-"""
     with open(os.path.join(repo_folder, "README.md"), "w") as f:
         f.write(yaml + model_card)
 
