b5c6e7034923b655ac29dc4ba3337b2d5b64ad1e
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-12-15 18:57:17 -0500

    cl args

--- ddpo_train_script.py
+++ ddpo_train_script.py
@@ -17,6 +17,7 @@ from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_a
 from huggingface_hub import create_repo, upload_folder
 from datasets import load_dataset
 import random
+import argparse
 
 def get_prompt_fn(dataset_name,split):
     hf_dataset=load_dataset(dataset_name,split=split)
@@ -26,3 +27,47 @@ def get_prompt_fn(dataset_name,split):
         return random.choice(prompt_list),{}
     
     return _fn
+
+parser = argparse.ArgumentParser(description="ddpo training")
+parser.add_argument(
+    "--pretrained_model_name_or_path",
+    type=str,
+    default=None,
+    required=True,
+    help="Path to pretrained model or model identifier from huggingface.co/models.",
+)
+parser.add_argument(
+    "--dataset_name",
+    type=str,
+    default=None,
+    help=(
+        "The name of the Dataset (from the HuggingFace hub) to train on"
+    ),
+)
+parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="/scratch/jlb638/sd-ddpo",
+        help="The output directory where the model predictions and checkpoints will be written.",
+)
+parser.add_argument("--seed", type=int, default=1234, help="A seed for reproducible training.")
+parser.add_argument(
+    "--hub_model_id",
+    type=str,
+    default=None,
+    help="The name of the repository on hf",
+)
+parser.add_argument(
+        "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
+)
+parser.add_argument("--num_train_epochs", type=int, default=10)
+parser.add_argument("--sample_num_steps",type=int,default=10, help="Number of sampler inference steps.")
+parser.add_argument("--sample_batch_size",type=int,default=10, help="batch size")
+parser.add_argument("--train_batch_size",type=int,default=10, help="actual batch size???")
+
+
+train_gradient_accumulation_steps=1,
+                sample_num_steps=2,
+                sample_batch_size=2,
+                train_batch_size=1,
+                sample_num_batches_per_epoch=2,
