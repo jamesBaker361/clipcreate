7d37f9b59bc139bc12e32eedf229fdc2c52fac1c
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-12-18 18:24:12 -0500

    added style list

--- static_globals.py
+++ static_globals.py
@@ -2,3 +2,6 @@ DATASET_NAME_MAPPING = {
     "lambdalabs/pokemon-blip-captions": ("image", "text"),
     "jlbaker361/sd-wikiart20": ("image", "text")
 }
+WIKIART_STYLES=[]
+with open("styles.txt","r+") as file:
+    WIKIART_STYLES=[f.strip() for f in file.readlines()]
