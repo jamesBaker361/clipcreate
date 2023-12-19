e409126b2eb509d1c21fa7cc59b34bdb83b0fc3d
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-12-18 19:19:33 -0500

    finished trainign script

--- ddpo_train_script.py
+++ ddpo_train_script.py
@@ -12,12 +12,14 @@ from transformers import CLIPProcessor, CLIPModel, CLIPVisionModel, CLIPTextMode
 from diffusers import DiffusionPipeline, StableDiffusionPipeline
 from torch.nn import Softmax
 import torch
+import time
 from creative_loss import clip_scorer_ddpo
 from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_aesthetic_model_filename
 from huggingface_hub import create_repo, upload_folder
 from datasets import load_dataset
 import random
 import argparse
+from static_globals import *
 
 def get_prompt_fn(dataset_name,split):
     hf_dataset=load_dataset(dataset_name,split=split)
@@ -32,17 +34,14 @@ parser = argparse.ArgumentParser(description="ddpo training")
 parser.add_argument(
     "--pretrained_model_name_or_path",
     type=str,
-    default=None,
-    required=True,
+    default="runwayml/stable-diffusion-v1-5",
     help="Path to pretrained model or model identifier from huggingface.co/models.",
 )
 parser.add_argument(
     "--dataset_name",
     type=str,
-    default=None,
-    help=(
-        "The name of the Dataset (from the HuggingFace hub) to train on"
-    ),
+    default="jlbaker361/wikiart-balanced100",
+    help="The name of the Dataset (from the HuggingFace hub) to train on"
 )
 parser.add_argument(
         "--output_dir",
@@ -55,19 +54,57 @@ parser.add_argument(
     "--hub_model_id",
     type=str,
     default=None,
-    help="The name of the repository on hf",
+    help="The name of the repository on hf to write to",
 )
-parser.add_argument(
-        "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
-)
-parser.add_argument("--num_train_epochs", type=int, default=10)
-parser.add_argument("--sample_num_steps",type=int,default=10, help="Number of sampler inference steps.")
-parser.add_argument("--sample_batch_size",type=int,default=10, help="batch size")
-parser.add_argument("--train_batch_size",type=int,default=10, help="actual batch size???")
+parser.add_argument("--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader.")
+parser.add_argument("--num_epochs", type=int, default=10)
+parser.add_argument("--sample_num_steps",type=int,default=10, help="Number of sampler inference steps per image")
+parser.add_argument("--sample_batch_size",type=int,default=4, help="batch size")
+parser.add_argument("--train_gradient_accumulation_steps", type=int, default=1)
+parser.add_argument("--style_list",nargs="*",help="styles to be used")
+parser.add_argument("--sample_num_batches_per_epoch",type=int,default=8)
 
+if __name__=='__main__':
+    args = parser.parse_args()
+    style_list=len(args.style_list)
+    if len(style_list)<2:
+        style_list=WIKIART_STYLES
+    reward_fn=clip_scorer_ddpo(style_list)
+    prompt_fn=get_prompt_fn(args.dataset_name)
 
-train_gradient_accumulation_steps=1,
-                sample_num_steps=2,
-                sample_batch_size=2,
-                train_batch_size=1,
-                sample_num_batches_per_epoch=2,
+    config=DDPOConfig(
+        num_epochs=args.num_epochs,
+        train_gradient_accumulation_steps=args.train_gradient_accumulation_steps,
+        sample_num_steps=args.sample_num_steps,
+        sample_batch_size=args.sample_batch_size,
+        train_batch_size=args.train_batch_size,
+        sample_num_batches_per_epoch=args.sample_num_batches_per_epoch,
+        mixed_precision="no"
+    )
+
+    pipeline = DefaultDDPOStableDiffusionPipeline(
+        args.pretrained_model_name_or_path,  use_lora=True
+    )
+
+    trainer = DDPOTrainer(
+            config,
+            reward_fn,
+            prompt_fn,
+            pipeline
+    )
+    start=time.time()
+    trainer.train()
+    end=time.time()
+    seconds=end-start
+    hours=seconds/(60*60)
+    print(f"successful training :) time elapsed: {seconds} seconds = {hours} hours")
+
+    trainer._save_pretrained(args.output_dir)
+    repo_id = create_repo(repo_id=args.hub_model_id, exist_ok=True).repo_id
+    upload_folder(
+        repo_id=repo_id,
+        folder_path=args.output_dir,
+        commit_message="End of training",
+        ignore_patterns=["step_*", "epoch_*"],
+    )
+    print("successful saving :)")
