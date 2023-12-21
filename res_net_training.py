aec808eb31f071b070b0f03ab1473900cb8bf8ec
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-21 12:19:46 -0500

    clargs

--- res_net_training.py
+++ res_net_training.py
@@ -6,3 +6,16 @@ os.environ["HF_HOME"]=cache_dir
 os.environ["HF_HUB_CACHE"]=cache_dir
 import torch
 torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
+from res_net_src import ResNet, HFDataset
+from accelerate import Accelerator
+import argparse
+parser = argparse.ArgumentParser(description="ddpo training")
+parser.add_argument("--epochs", type=int,default=100)
+parser.add_argument("--dataset_name",type=str,default="jlbaker361/wikiart")
+parser.add_argument("--pretrained_version",type=str, default="resnet18")
+def training_loop(epochs:int, dataset_name:str, pretrained_version:str):
+    pass
+
+if __name__=="__main__":
+    args=parser.parse_args()
+    training_loop(args.epochs, args.dataset_name,args.pretrained_version)
