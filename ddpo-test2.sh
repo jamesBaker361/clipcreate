ee893c7c53de291cfad4035e0796a3a1ca98b10b
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-29 18:16:37 -0500

    mixed precision testing

--- ddpo-test2.sh
+++ ddpo-test2.sh
@@ -6,4 +6,18 @@ python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wiki
   --hub_model_id "jlbaker361/sd-lora0" \
   --train_gradient_accumulation_steps 2 \
   --train_batch_size 2 --num_epochs 2 --sample_num_steps 2  \
-  --sample_batch_size 2  --sample_num_batches_per_epoch 4 \
+  --sample_batch_size 2  --sample_num_batches_per_epoch 4  > vanilla.txt 2>&1
+
+  python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-balanced100" \
+  --image_dir "/home/ubuntu/images/" --output_dir "sd-lora0fp16" \
+  --hub_model_id "jlbaker361/sd-lora0fp16" \
+  --train_gradient_accumulation_steps 2 \
+  --train_batch_size 2 --num_epochs 2 --sample_num_steps 2  \
+  --sample_batch_size 2  --sample_num_batches_per_epoch 4 --mixed_precision "fp16" > fp16.txt 2>&1
+
+  python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-balanced100" \
+  --image_dir "/home/ubuntu/images/" --output_dir "sd-lora0" \
+  --hub_model_id "jlbaker361/sd-lora0" \
+  --train_gradient_accumulation_steps 2 \
+  --train_batch_size 2 --num_epochs 2 --sample_num_steps 2  \
+  --sample_batch_size 2  --sample_num_batches_per_epoch 4 --mixed_precision "bf16" > bf16.txt 2>&1
