c4ae5414f972bc565cf173d90570e5538cd579ca
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-29 18:27:15 -0500

    conda init needed??

--- startup-python.sh
+++ startup-python.sh
@@ -1,4 +1,5 @@
 yes | conda create -n creativity python=3.11
+conda init
 conda activate creativity
 python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python3 -m pip install wandb huggingface_hub peft diffusers bitsandbytes scipy
