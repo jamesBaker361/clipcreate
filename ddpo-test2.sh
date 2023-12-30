2f6139cf1d963d743c65827cb54d94bde3be813b
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2023-12-30 14:03:39 -0500

    shorter test

--- ddpo-test2.sh
+++ ddpo-test2.sh
@@ -1,9 +1,6 @@
-
-
-
-python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-balanced100" \
-  --image_dir "/home/ubuntu/images/" --output_dir "sd-lora0" \
-  --hub_model_id "jlbaker361/sd-lora0" \
-  --train_gradient_accumulation_steps 2 \
-  --train_batch_size 2 --num_epochs 2 --sample_num_steps 2  \
-  --sample_batch_size 2  --sample_num_batches_per_epoch 4
+python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-0epoch" \
+  --image_dir "/home/user/vanilla-images/" --output_dir "sd-wikiart-lora-0epoch-ddpo" \
+  --hub_model_id "jlbaker361/sd-wikiart-lora-0epoch-ddpo" \
+  --train_gradient_accumulation_steps 4 \
+  --train_batch_size 4 --num_epochs 2 --sample_num_steps 30  \
+  --sample_batch_size 4  --sample_num_batches_per_epoch 8
