be7d81967d7682c50f383b0a169b1285d114a6a5
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-29 19:07:30 -0500

    deleted garbage

--- ddpo-bf16.sh
+++
@@ -1,6 +0,0 @@
-  python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-balanced100" \
-  --image_dir "/home/ubuntu/images/" --output_dir "sd-lora0" \
-  --hub_model_id "jlbaker361/sd-lora0" \
-  --train_gradient_accumulation_steps 2 \
-  --train_batch_size 2 --num_epochs 2 --sample_num_steps 2  \
-  --sample_batch_size 2  --sample_num_batches_per_epoch 4 --mixed_precision "bf16"
