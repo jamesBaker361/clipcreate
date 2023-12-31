67a3b726267e87c73c2189c5f35f0b02a902c848
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2023-12-31 10:46:16 -0500

    image dirs

--- ddpo-ft.sh
+++ ddpo-ft.sh
@@ -1,5 +1,5 @@
 python3 ddpo_train_script.py --pretrained_model_name_or_path "jlbaker361/sd-wikiart-lora-balanced100" \
-  --image_dir "/home/user/ft-images/" --output_dir "sd-wikiart-lora-balanced100-ddpo" \
+  --image_dir "~/ft-images/" --output_dir "sd-wikiart-lora-balanced100-ddpo" \
   --hub_model_id "jlbaker361/sd-wikiart-lora-balanced100-ddpo" \
   --train_gradient_accumulation_steps 4 \
   --train_batch_size 4 --num_epochs 20 --sample_num_steps 30  \
