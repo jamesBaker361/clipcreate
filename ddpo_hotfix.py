95b027be2635d1c26ae3dace7506ff09c3dec59c
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-01-20 00:19:21 -0500

    model uploading

--- ddpo_hotfix.py
+++ ddpo_hotfix.py
@@ -28,3 +28,41 @@ if __name__=='__main__':
     print("loading???")
     pipeline.sd_pipeline.load_lora_weights(resume_from,weight_name="pytorch_lora_weights.safetensors")
     print("loaded :)")
+
+    config=DDPOConfig(
+        num_epochs=0
+    )
+    def get_prompt_fn(dataset_name,split):
+        hf_dataset=load_dataset(dataset_name,split=split)
+        prompt_list=[t for t in hf_dataset["text"]]
+
+        def _fn():
+            return random.choice(prompt_list),{}
+        return _fn
+
+    reward_fn=clip_scorer_ddpo(["dw about it"])
+    prompt_fn=get_prompt_fn("jlbaker361/wikiart-balanced250", "train")
+
+    trainer = DDPOTrainer(
+            config,
+            reward_fn,
+            prompt_fn,
+            pipeline
+    )
+
+    trainer._save_pretrained("vanilla-ddpo25/")
+    repo_id = create_repo(repo_id="jlbaker361/vanilla-ddpo25", exist_ok=True).repo_id
+    upload_folder(
+        repo_id=repo_id,
+        folder_path=args.output_dir,
+        commit_message="End of training",
+        ignore_patterns=["step_*", "epoch_*"],
+    )
+    model_card_content=f"""
+    # DDPO trained model
+    pls train for one more epoch
+
+    """
+    card=ModelCard(model_card_content)
+    card.push_to_hub(repo_id)
+    print("successful saving :)")
