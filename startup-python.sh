8d5e4ea3c82ef0eeffb995871748dc09474e1293
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-01-26 09:29:46 -0500

    add matplotlib

--- startup-python.sh
+++ startup-python.sh
@@ -2,5 +2,5 @@ yes | conda create -n creativity python=3.11
 conda init
 conda activate creativity
 python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python3 -m pip install wandb huggingface_hub peft diffusers bitsandbytes scipy
+python3 -m pip install wandb huggingface_hub peft diffusers bitsandbytes scipy matplotlib
 python3 -m pip install git+https://github.com/jamesBaker361/trl-fixed.git
