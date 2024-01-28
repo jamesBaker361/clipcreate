adf3440f6c0e43c2c44d2bf59a56f92b3e19203b
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-01-28 13:52:55 -0500

    printing

--- creative_loss.py
+++ creative_loss.py
@@ -41,6 +41,7 @@ def clip_scorer_ddpo(style_list): #https://github.com/huggingface/trl/blob/main/
     @torch.no_grad()
     def _fn(images, prompts, metadata):
         inputs = processor(text=style_list, images=images, return_tensors="pt", padding=True)
+        print(inputs)
         outputs = model(**inputs)
         logits_per_image = outputs.logits_per_image # this is the image-text similarity score
         probs = logits_per_image.softmax(dim=1)
