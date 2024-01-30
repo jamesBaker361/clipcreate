0aa36f13b4f23d71f31a4ea66d519f13c89b0831
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-01-30 10:54:10 -0500

    startup script

--- startup-python.sh
+++ startup-python.sh
@@ -1,6 +1,8 @@
-yes | conda create -n creativity python=3.11
-conda init
-conda activate creativity
+# before this you shoudl run:
+# yes | conda create -n creativity python=3.11
+# conda init
+# conda activate creativity
+
 yes |  conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 yes |  conda install -c conda-forge  scipy matplotlib datasets
 yes |  conda install -c conda-forge transformers
