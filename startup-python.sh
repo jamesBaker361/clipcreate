05f5b1573cc81e8e4ed1e770473a3b576fb35d26
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-01-29 19:53:21 -0500

    new startup script with conda

--- startup-python.sh
+++ startup-python.sh
@@ -1,6 +1,12 @@
 yes | conda create -n creativity python=3.11
 conda init
 conda activate creativity
-python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python3 -m pip install wandb huggingface_hub peft diffusers bitsandbytes scipy matplotlib trl
-#python3 -m pip install git+https://github.com/jamesBaker361/trl-fixed.git
+yes |  conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+yes |  conda install -c conda-forge  scipy matplotlib datasets
+yes |  conda install -c conda-forge transformers
+yes |  conda install -c conda-forge huggingface_hub
+yes |  conda install -c conda-forge diffusers
+yes |  conda install -c conda-forge wandb
+yes |  conda install -c conda-forge bitsandbytes
+yes |  conda install -c conda-forge peft
+pip install trl["diffusers"]
