ea85b27ba394258273ad0231ac1acba9d4d64772
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-01 16:55:55 -0500

    who cares

--- appendix.sh
+++ appendix.sh
@@ -1,2 +1,4 @@
 #sbatch runpygpu.sh appendix_images.py --limit 5 --can_model_list "jlbaker361/dcgan-lazy-wikiart1000-resized" "jlbaker361/dcgan-lazy-wikiart500-clip-resized" "jlbaker361/dcgan-lazy-wikiart1000-resized" --conditional True
-sbatch runpygpu.sh appendix_images.py --limit 5 --can_model_list "jlbaker361/dcgan-wikiart1000-resized" "jlbaker361/dcgan-wikiart1000-clip-resized"
+#sbatch runpygpu.sh appendix_images.py --limit 5 --can_model_list "jlbaker361/dcgan-wikiart1000-resized" "jlbaker361/dcgan-wikiart1000-clip-resized"
+sbatch runpygpu.sh appendix_images.py --limit 5 --ddpo_model_list "jlbaker361/ddpo-stability" "jlbaker361/ddpo-stability-dcgan" --conditional True --file_path "table-ddpo.png"
+sbatch runpygpu.sh appendix_images.py --limit 5 --ddpo_model_list "jlbaker361/ddpo-stability" "jlbaker361/ddpo-stability-dcgan" --file_path "table-ddpo-cond.png"
