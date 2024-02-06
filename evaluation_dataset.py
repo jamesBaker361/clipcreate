c7b83555abceec6b15f87d82859afaf4f6a34778
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-06 09:29:44 -0500

    print results

--- evaluation_dataset.py
+++ evaluation_dataset.py
@@ -10,6 +10,8 @@ torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
 from torchmetrics.image.inception import InceptionScore
 from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_aesthetic_model_filename
 from datasets import Dataset,load_dataset
+from torchvision.transforms import PILToTensor
+import numpy as np
 
 import argparse
 parser = argparse.ArgumentParser(description="download and evaluate a dataset")
@@ -17,6 +19,7 @@ parser.add_argument("--dataset_name",type=str, help="dataset to download from hf
 
 def evaluate(args):
     hf_dataset=load_dataset(args.dataset_name,split="train")
+    inception = InceptionScore(normalize=True)
     model_image_dict={}
     model_score_dict={}
     for row in hf_dataset:
@@ -27,6 +30,19 @@ def evaluate(args):
         if model_name not in model_score_dict:
             model_score_dict[model_name]=[]
         pil_img=row["image"]
+        tensor_img=PILToTensor()(pil_img)
+        model_image_dict.append(tensor_img)
+        model_score_dict.append(ava_score)
+    for model in model_image_dict.keys():
+        image_list=model_image_dict[model]
+        image_tensor=torch.cat(image_list)
+        inception.update(image_tensor)
+        inception_mean, inception_std=inception.compute()
+        print(model)
+        print("inception mean", inception_mean, "inceptstion std", inception_std)
+        score_list=model_score_dict[model]
+        print("ava mean", np.mean(score_list), "ava std dev", np.std(score_list))
+
 
 if __name__=='__main__':
     args=parser.parse_args()
