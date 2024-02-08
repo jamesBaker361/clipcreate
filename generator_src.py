8bc08d4aebbeb80f774bd3ff55316de7522de2ef
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-07 23:05:22 -0500

    unused imports gen

--- generator_src.py
+++ generator_src.py
@@ -1,18 +1,6 @@
-import argparse
-import os
-import random
-import torch
+
 import torch.nn as nn
-import torch.nn.parallel
-import torch.optim as optim
-import torch.utils.data
-import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from IPython.display import HTML
 
 #generator: noise~N(0,1) -> Image 0-255
 
