868d9da276b4496456f6b90ddb855df1282cec6b
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-22 20:45:03 -0500

    finished up scriptings

--- lora_scale_comparison.py
+++ lora_scale_comparison.py
@@ -7,10 +7,14 @@ os.environ["HF_HUB_CACHE"]=cache_dir
 from trl import DDPOConfig, DDPOTrainer, DefaultDDPOStableDiffusionPipeline
 import torchvision.transforms as transforms
 import torch
+torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
+from torchmetrics.image.inception import InceptionScore
+import numpy as np
 import wandb
 import argparse
 from datasets import Dataset
 from huggingface_hub import upload_file
+from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_aesthetic_model_filename
 
 parser = argparse.ArgumentParser(description="comparison of lora scales")
 parser.add_argument("--seed",type=int,default=0, help="random seed")
@@ -22,6 +26,7 @@ parser.add_argument("--dir",type=str,default="lora_scale_comp")
 parser.add_argument("--repo_id",type=str,default="jlbaker361/lora_scale_comparison")
 
 def main(args):
+
     generator = torch.Generator(device="cpu").manual_seed(args.seed)
     lora_generator= torch.Generator(device="cpu").manual_seed(args.seed)
     lora_scale_generator=torch.Generator(device="cpu").manual_seed(args.seed)
@@ -35,36 +40,68 @@ def main(args):
     SCALE="scaled_lora"
     LORA="lora"
     VANILLA="vanilla"
+    VANILLA_SCORE="vanilla_score"
+    LORA_SCORE="lora_score"
+    SCALE_SCORE="scaled_lora_score"
 
     src_dict={
         SCALE:[],
         LORA:[],
-        VANILLA:[]
+        VANILLA:[],
+        VANILLA_SCORE:[],
+        LORA_SCORE:[],
+        SCALE_SCORE:[]
     }
 
     run=wandb.init(project="lora scale comparison")
+    aesthetic_fn=aesthetic_scorer(hf_hub_aesthetic_model_id, hf_hub_aesthetic_model_filename)
 
     for n in range(args.n_images):
         img_vanilla=pipeline(" ",num_inference_steps=args.num_inference_steps,generator=generator).images[0]
         vanilla_path=f"{args.dir}/img{n}.jpg"
         img_vanilla.save(vanilla_path)
+        img_vanilla_score=aesthetic_fn(img_vanilla,{},{})[0]
+
         img_lora=lora_pipeline(" ", num_inference_steps=args.num_inference_steps,generator=lora_generator).images[0]
         lora_path=f"{args.dir}/img{n}_lora.jpg"
         img_lora.save(lora_path)
+        img_lora_score=aesthetic_fn(img_lora,{},{})[0]
+
         lora_scale_path=f"{args.dir}/img{n}_lora_scaled.jpg"
         img_lora_scale=lora_pipeline(" ", num_inference_steps=args.num_inference_steps,generator=lora_scale_generator,cross_attention_kwargs={"scale": 3.0}).images[0]
         img_lora_scale.save(lora_scale_path)
+        img_lora_scale_score=aesthetic_fn(img_lora_scale,{},{})[0]
 
         run.log({VANILLA: wandb.Image(vanilla_path)},step=n)
         run.log({LORA: wandb.Image(lora_path)},step=n)
         run.log({SCALE: wandb.Image(lora_scale_path) },step=n)
 
+        run.log({VANILLA_SCORE: img_vanilla_score},step=n)
+        run.log({LORA_SCORE:img_lora_score},step=n)
+        run.log({SCALE_SCORE: img_lora_scale_score},step=n)
+
         src_dict[VANILLA].append(img_vanilla)
         src_dict[LORA].append(img_lora)
         src_dict[SCALE].append(img_lora_scale)
+
+        src_dict[VANILLA_SCORE].append(img_vanilla_score)
+        src_dict[SCALE_SCORE].append(img_lora_scale_score)
+        src_dict[LORA_SCORE].append(img_lora_score)
+
+    
     Dataset.from_dict(src_dict).push_to_hub(args.repo_id)
-    model_card_content=f"created a total of {args.n_images} images \n"
-    model_card_content+=f"wandb run url: {run.get_url()}\n"
+    model_card_content=f"created a total of {args.n_images} images \n\n"
+    model_card_content+=f"wandb run url: {run.get_url()}\n\n"
+    for key in [VANILLA,SCALE,LORA]:
+        inception = InceptionScore(normalize=True)
+        tensor_stack=torch.stack([transforms.PILToTensor()(img) for img in  src_dict[key]])
+        inception.update(tensor_stack)
+        inception_mean, inception_std=inception.compute()
+        model_card_content+=f"{key} inception mean: {inception_mean} std: {inception_std}\n\n"
+        score_list=src_dict[key+"_score"]
+        ava_std=np.std(score_list)
+        ava_mean=np.mean(score_list)
+        model_card_content+=f"{key} ava mean: {ava_mean} std: {ava_std}\n\n"
     with open("tmp_lora_scale.md","w+") as file:
         file.write(model_card_content)
     upload_file(path_or_fileobj="tmp_lora_scale.md", 
@@ -77,3 +114,4 @@ if __name__=='__main__':
     args=parser.parse_args()
     print(args)
     main(args)
+    print("all done :)")
--- negative_prompting.py
+++ negative_prompting.py
@@ -7,9 +7,16 @@ os.environ["HF_HUB_CACHE"]=cache_dir
 from trl import DDPOConfig, DDPOTrainer, DefaultDDPOStableDiffusionPipeline
 import torchvision.transforms as transforms
 import torch
+torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
+from torchmetrics.image.inception import InceptionScore
 from accelerate import Accelerator
+from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_aesthetic_model_filename
+import numpy as np
+
 import wandb
 from static_globals import *
+from datasets import Dataset
+from huggingface_hub import upload_file
 torch.manual_seed(0)
 import argparse
 
@@ -19,11 +26,14 @@ parser.add_argument("--dir",type=str,default="images")
 parser.add_argument("--n_images",type=int,default=10)
 parser.add_argument("--prompt",type=str,default=" ")
 parser.add_argument("--base_model",type=str, default="stabilityai/stable-diffusion-2-base")
+parser.add_argument("--seed",type=int,default=0)
+parser.add_argument("--repo_id",type=str,default="jlbaker361/negative_creativity")
 
 #lora_generator= torch.Generator(device="cpu").manual_seed(0)
 
 from call_neg import call_multi_neg, call_vanilla
 def main(args):
+    aesthetic_fn=aesthetic_scorer(hf_hub_aesthetic_model_id, hf_hub_aesthetic_model_filename)
     accelerator = Accelerator(log_with="wandb")
     accelerator.init_trackers(project_name="negative_prompting",init_kwargs={
             "wandb":
@@ -40,16 +50,63 @@ def main(args):
     #pipeline.sd_pipeline=accelerator.prepare(pipeline.sd_pipeline)
     #generator = torch.Generator(device=accelerator.device).manual_seed(0)
 
+    NEGATIVE="negative_image"
+    VANILLA="vanilla_image"
+    NEGATIVE_SCORE=NEGATIVE+"_score"
+    VANILLA_SCORE=VANILLA+"_score"
+    src_dict={
+        NEGATIVE:[],
+        VANILLA:[],
+        VANILLA_SCORE:[],
+        NEGATIVE_SCORE:[]
+    }
+    generator = torch.Generator(device="cpu").manual_seed(args.seed)
+    neg_generator= torch.Generator(device="cpu").manual_seed(args.seed)
+
+
     for x in range(args.n_images):
         neg_img=call_multi_neg(pipeline.sd_pipeline,"painting",num_inference_steps=args.num_inference_steps,
-                        negative_prompt=WIKIART_STYLES).images[0]
+                               generator=neg_generator,
+                        negative_prompt=["horse","donkey"]).images[0]
         path=f"{args.dir}/img{x}.png"
         neg_img.save(path)
-        accelerator.log({"neg_image": wandb.Image(path)},step=x)
-        vanilla_img=pipeline("painting",num_inference_steps=args.num_inference_steps).images[0]
+        neg_img_score=aesthetic_fn(neg_img,{},{})[0]
+
+        accelerator.log({NEGATIVE: wandb.Image(path)},step=x)
+        accelerator.log({NEGATIVE_SCORE:neg_img_score},step=x)
+        src_dict[NEGATIVE].append(neg_img)
+        src_dict[NEGATIVE_SCORE].append(neg_img_score)
+
+        vanilla_img=pipeline("painting",num_inference_steps=args.num_inference_steps,generator=generator).images[0]
         path=f"{args.dir}/vanilla_img{x}.png"
         vanilla_img.save(path)
-        accelerator.log({"vanilla_image": wandb.Image(path)},step=x)
+        vanilla_img_score=aesthetic_fn(vanilla_img,{},{})[0]
+
+        accelerator.log({VANILLA: wandb.Image(path)},step=x)
+        accelerator.log({VANILLA_SCORE: vanilla_img_score},step=x)
+        src_dict[VANILLA].append(vanilla_img)
+        src_dict[VANILLA_SCORE].append(vanilla_img_score)
+
+    run_url=accelerator.get_tracker("wandb").run.get_url()
+    Dataset.from_dict(src_dict).push_to_hub(args.repo_id)
+    model_card_content=f"created a total of {args.n_images} images \n\n"
+    model_card_content+=f"wandb run url: {run_url}\n\n"
+    for key in [NEGATIVE,VANILLA]:
+        inception = InceptionScore(normalize=True)
+        tensor_stack=torch.stack([transforms.PILToTensor()(img) for img in  src_dict[key]])
+        inception.update(tensor_stack)
+        inception_mean, inception_std=inception.compute()
+        model_card_content+=f"{key} inception mean: {inception_mean} std: {inception_std}\n\n"
+        score_list=src_dict[key+"_score"]
+        ava_std=np.std(score_list)
+        ava_mean=np.mean(score_list)
+        model_card_content+=f"{key} ava mean: {ava_mean} std: {ava_std}\n\n"
+    with open("tmp_neg.md","w+") as file:
+        file.write(model_card_content)
+    upload_file(path_or_fileobj="tmp_neg.md", 
+                path_in_repo="README.md",
+                repo_id=args.repo_id,
+                repo_type="dataset")
 
 if __name__=='__main__':
     args=parser.parse_args()
