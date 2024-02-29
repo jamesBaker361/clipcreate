b45fcf59117b648dd7aadc6142ffff26b55bfa1f
James Baker <jlbaker361@gmail.com>
2024-02-29 13:25:31 -0500

    nits idk

--- ddpo_test.py
+++ ddpo_test.py
@@ -16,6 +16,10 @@ import torch
 from creative_loss import clip_scorer_ddpo
 from aesthetic_reward import aesthetic_scorer,hf_hub_aesthetic_model_id,hf_hub_aesthetic_model_filename
 from huggingface_hub import create_repo, upload_folder
+from safetensors import safe_open
+from safetensors.torch import save_file
+from peft import get_peft_model_state_dict
+from better_pipeline import BetterDefaultDDPOStableDiffusionPipeline
 
 def prompt_fn():
     return "a painting", {}
@@ -23,25 +27,48 @@ def prompt_fn():
 clip_reward_fn=clip_scorer_ddpo(["baroque", "cubism"])
 aesthetic_reward_fn=aesthetic_scorer(hf_hub_aesthetic_model_id, hf_hub_aesthetic_model_filename)
 
+def save_lora_weights(pipeline:BetterDefaultDDPOStableDiffusionPipeline,output_dir:str):
+    state_dict=get_peft_model_state_dict(pipeline.sd_pipeline.unet, unwrap_compiled=True)
+    weight_path=os.path.join(output_dir, "pytorch_lora_weights.safetensors")
+    print("saving to ",weight_path)
+    save_file(state_dict, weight_path, metadata={"format": "pt"})
+
+def load_lora_weights(pipeline:BetterDefaultDDPOStableDiffusionPipeline,output_dir:str):
+    #pipeline.get_trainable_layers()
+    path=os.path.join(output_dir, "pytorch_lora_weights.safetensors")
+    print("loading from ",path)
+    state_dict={}
+    count=0
+    with safe_open(path, framework="pt", device="cpu") as f:
+        for key in f.keys():
+            state_dict[key]=f.get_tensor(key)
+    state_dict={
+        k.replace("weight","default.weight"):v for k,v in state_dict.items()
+    }
+    pipeline.sd_pipeline.unet.load_state_dict(state_dict,strict=False)
+
 def train_test(mixed_precision="no", 
                reward_fn=clip_reward_fn,
-               src_hub_model_id="jlbaker361/sd-wikiart20",
+               src_hub_model_id="runwayml/stable-diffusion-v1-5",
                hub_model_id="jlbaker361/sd-ddpo-wikiart20",
                save_directory="/scratch/jlb638/sd-ddpo-test"):
 
+    weight_name="down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.lora_A.default.weight"
     config=DDPOConfig(
         num_epochs=1,
                 train_gradient_accumulation_steps=1,
                 sample_num_steps=2,
-                sample_batch_size=2,
+                sample_batch_size=1,
                 train_batch_size=1,
-                sample_num_batches_per_epoch=2,
-                mixed_precision=mixed_precision
+                sample_num_batches_per_epoch=1,
+                mixed_precision=mixed_precision,
+                train_learning_rate=0.03
     )
-
-    pipeline = DefaultDDPOStableDiffusionPipeline(
+    pipeline = BetterDefaultDDPOStableDiffusionPipeline(
             src_hub_model_id,  use_lora=True
         )
+    print("before training:")
+    print(pipeline.sd_pipeline.unet.get_parameter('down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.lora_A.default.weight').detach().numpy()[0][:10])
 
     trainer = DDPOTrainer(
             config,
@@ -51,6 +78,20 @@ def train_test(mixed_precision="no",
         )
     trainer.train()
     print("successful training boys :)))")
+    print("after training:")
+    print(pipeline.sd_pipeline.unet.get_parameter('down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.lora_A.default.weight').detach().numpy()[0][:10])
+    save_lora_weights(pipeline, save_directory)
+    load_lora_weights(pipeline, save_directory)
+    pipeline = BetterDefaultDDPOStableDiffusionPipeline(
+            src_hub_model_id,  use_lora=True
+        )
+    print("before loading:")
+    print(pipeline.sd_pipeline.unet.get_parameter('down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.lora_A.default.weight').detach().numpy()[0][:10])
+    
+    load_lora_weights(pipeline, save_directory)
+    print("after loading")
+    print(pipeline.sd_pipeline.unet.get_parameter('down_blocks.0.attentions.0.transformer_blocks.0.attn1.to_q.lora_A.default.weight').detach().numpy()[0][:10])
+    exit()
     trainer._save_pretrained(save_directory)
     repo_id = create_repo(repo_id=hub_model_id, exist_ok=True).repo_id
     upload_folder(
