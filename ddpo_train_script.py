e28239acfcc424c72c0bc16fae0e2f9a3eca77bd
James Baker <jlbaker361@gmail.com>
2024-03-04 21:28:21 -0500

    hub cache

--- ddpo_train_script.py
+++ ddpo_train_script.py
@@ -1,12 +1,12 @@
 import os
 import sys
-cache_dir="/scratch/jlb638/trans_cache"
-os.environ["TRANSFORMERS_CACHE"]=cache_dir
-os.environ["HF_HOME"]=cache_dir
-os.environ["HF_HUB_CACHE"]=cache_dir
+#cache_dir="/scratch/jlb638/trans_cache"
+#os.environ["TRANSFORMERS_CACHE"]=cache_dir
+#os.environ["HF_HOME"]=cache_dir
+#os.environ["HF_HUB_CACHE"]=cache_dir
 
 import torch
-torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
+#torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
 import argparse
 from static_globals import *
 import faulthandler
