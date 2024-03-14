4640ef8e9eae38fffbcbb4fe59b6509229da3ace
James Baker <jlbaker361@gmail.com>
2024-03-14 11:42:20 -0400

    i do not trust xformers

--- ddpo_train_script.py
+++ ddpo_train_script.py
@@ -194,16 +194,6 @@ if __name__=='__main__':
     if args.image_dir==None:
         args.image_dir="images"
         os.makedirs(args.image_dir, exist_ok=True)
-    if is_xformers_available():
-        print("xformers?!?!")
-        import xformers
-
-        xformers_version = version.parse(xformers.__version__)
-        if xformers_version == version.parse("0.0.16"):
-            print("xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details.")
-        pipeline.sd_pipeline.unet.enable_xformers_memory_efficient_attention()
-    else:
-        print("xformers is not available. Make sure it is installed correctly")
     image_samples_hook=get_image_sample_hook(args.image_dir)
     for e in range(start_epoch,args.num_epochs):
         config=DDPOConfig(
