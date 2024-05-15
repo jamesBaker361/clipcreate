9b260e262b201365fdb74748709804dd7fe40a3c
James Baker <jlbaker361@gmail.com>
2024-05-14 23:16:02 -0400

    slurm deletions

--- ddpo_train_script.py
+++ ddpo_train_script.py
@@ -1,14 +1,6 @@
 import os
 import sys
 import torch
-cache_dir="/scratch/jlb638/trans_cache"
-os.environ["TRANSFORMERS_CACHE"]=cache_dir
-os.environ["HF_HOME"]=cache_dir
-os.environ["HF_HUB_CACHE"]=cache_dir
-os.environ["WANDB_DIR"]="/scratch/jlb638/wandb"
-os.environ["WANDB_CACHE_DIR"]="/scratch/jlb638/wandb_cache"
-
-torch.hub.set_dir("/scratch/jlb638/torch_hub_cache")
 import argparse
 from static_globals import *
 import faulthandler
