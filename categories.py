096da48a4a1a24d61cd3150e90f16ea3b536ce02
James Baker <jlbaker361@gmail.com>
2024-07-20 10:56:27 -0400

    ???

--- categories.py
+++ categories.py
@@ -1,7 +1,7 @@
 #use this to find only the top ten classes
 
 import os
-from  datasets import load_dataset
+from  datasets import load_dataset,Dataset
 
 data=load_dataset("jlbaker361/wikiart",split="train")
 
@@ -34,7 +34,7 @@ def get_word_dict(word_list)->dict:
 
 count_dict=get_count_dict()
 
-for word_list in [medium_list, subject_list]:
+def get_top_ten(word_list):
     print(word_list)
     word_dict=get_word_dict(word_list)
     normalized_list=[
@@ -42,4 +42,22 @@ for word_list in [medium_list, subject_list]:
     ]
     normalized_list.sort(key=lambda x: x[1],reverse=True)
     for n in normalized_list:
-        print("\t",n)
+        print("\t",n[0], "&", round(100* n[1],2), "&" ,count_dict[n[0]]," \\\\")
+    return [n[0] for n in normalized_list[:10]]
+
+def make_data(name,word_list):
+    top_ten=get_top_ten(word_list)
+    src_dict={
+        "image":[],
+        "text":[],
+        "style":[]
+    }
+    for row in data:
+        if row["style"] in top_ten:
+            for key in ["image","text","style"]:
+                src_dict[key].append(row[key])
+    #Dataset.from_dict(src_dict).push_to_hub(name)
+    load_dataset(name)
+
+make_data("jlbaker361/wikiart-mediums",medium_list)
+make_data("jlbaker361/wikiart-subjects",subject_list)
