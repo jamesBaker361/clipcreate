06ac1790f366e5d4cecaaf6da3823e92dc8738dd
James Baker <jlbaker361@gmail.com>
2025-04-14 11:51:28 -0400

    diversity analysis

---
+++ analysis_april.py
@@ -0,0 +1,18 @@
+import diffusers
+import huggingface_hub
+from PIL import Image
+import pytorch_image_generation_metrics
+import os
+from image_diversity import ClipMetrics
+
+clip_metrics = ClipMetrics()
+
+
+data=[f"F{x}" for x in range(3)]+[f"M{x}" for x in range(3)]
+
+def calculate_mode_collapse(image_list:list[Image.Image]):
+    dir_name="temp"
+    os.makedirs(dir_name,exist_ok=True)
+    for i,image in enumerate(image_list):
+        image.save(f"{dir_name}/{i}.png")
+    tce = clip_metrics.tce(dir_name)
